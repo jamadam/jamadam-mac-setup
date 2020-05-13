104a0e667d4ceced767d39c28daadde71f239f2d
Keita Jamadam Sugama <sugama@jamadam.com>
2020-05-13 11:31:00 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,9 +13,7 @@ brew cask reinstall virtualbox
 brew cask reinstall vagrant
 brew cask reinstall firefox
 brew cask reinstall google-chrome
-brew cask reinstall libreoffice
 brew cask reinstall google-hangouts
-brew cask reinstall komodo-edit
 brew cask reinstall sourcetree
 brew cask reinstall thunderbird
 brew cask reinstall mysqlworkbench
@@ -24,18 +22,21 @@ brew cask reinstall trolcommander
 brew cask reinstall keka
 brew cask reinstall jedit
 brew cask reinstall imageoptim
-brew cask reinstall limechat
-brew cask reinstall skype
-brew cask reinstall 0xed
-brew cask reinstall atom
 brew cask install remote-desktop-connection
-brew cask install db-browser-for-sqlite
-brew reinstall azure-cli
 brew reinstall openssl
 brew reinstall gimp
 brew cask install docker
 brew cask install zoomus
-brew caks install onyx
+
+#brew cask reinstall libreoffice
+#brew cask reinstall komodo-edit
+#brew cask reinstall limechat
+#brew cask reinstall skype
+#brew cask reinstall 0xed
+#brew cask reinstall atom
+#brew caks install onyx
+#brew cask install db-browser-for-sqlite
+#brew reinstall azure-cli
 
 brew cleanup
 brew cask cleanup
