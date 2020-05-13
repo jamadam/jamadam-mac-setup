6cf843fed96e4ebf8d027dfd9da64f2b2fd33c2c
Keita Jamadam Sugama <sugama@jamadam.com>
2020-05-13 11:24:05 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 #
 
 # Homebrew をインストール
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 brew update
 brew cask reinstall iterm2
